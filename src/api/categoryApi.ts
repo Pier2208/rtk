3d49f6a33ecb85dda3e499348328f8736d0978b7
Pier2208 <p.leroy.mtl@gmail.com>
2024-07-05 15:49:33 -0400

    fix:add tags to categories

--- src/api/categoryApi.ts
+++ src/api/categoryApi.ts
@@ -4,9 +4,11 @@ import { Category } from '../types/Category';
 export const categoryApi = createApi({
   reducerPath: 'categoryApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  tagTypes: ['Categories'],
   endpoints: (builder) => ({
     getCategories: builder.query<Category[], void>({
-      query: () => '/categories'
+      query: () => '/categories',
+      providesTags: ['Categories']
     }),
   }),
 });
